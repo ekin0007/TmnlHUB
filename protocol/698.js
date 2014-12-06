3fa4f97fbb0a4714b331914baeccd6a1ad9bfc44
Rango <baiej214@Gmail.com>
2014-12-06 11:07:14 +0800

    新增一些函数

--- protocol/698.js
+++ protocol/698.js
@@ -43,32 +43,49 @@ var json_hex = {
     },
 
     set_len = function (data) {
-        return [0x00, 0x00, 0x00, 0x00];
+        var len = (data.length << 2) + 2, buff = new Buffer(4);
+        buff.writeInt16LE(len, 0);
+        buff.writeInt16LE(len, 2);
+        return buff.toJSON();
     },
 
-    set_c = function () {
-        return [0x00];
-    },
 
-    set_addr = function (A1, A2, A3) {
-        return [0x00, 0x00, 0x00, 0x00, 0x00];
-    },
-
-    set_seq = function (seq) {
-        if (!seq && seq != 0) {
-            seq = 0;
-        } else {
-            seq = seq >= 15 ? 0 : seq++;
+    set_c = function (json) {
+        var c = json.C, afn = json.AFN, cfn = 11,
+            dir = c.DIR, prm = c.PRM, fcb = c.FCB, fcv = c.FCV;
+        if (afn == 1) {
+            fcb = 0;
+            cfn = 1;
+        } else if (_.has([4, 5, 15], afn)) {
+            cfn = 10;
         }
-        return [seq];
+        return [(parseInt('' + dir + prm + fcb + fcv, 2) << 4) + cfn];
+    },
+
+    set_addr = function (a1, a2, a3) {
+        return [tools.b2bcd(a1 % 100), tools.b2bcd(Math.floor(a1 / 100)), a2 % 256, Math.floor(a2 / 256), a3];
+    },
+
+    set_seq = function (json) {
+        var afn = json.AFN, tpv = 0, fir = 1, fin = 1, con = 0, seq = json.seq || 0;
+        if (_.has([4, 5, 15], afn)) {
+            con = 1;
+        }
+        seq = seq >= 15 ? 0 : seq++;
+        return [(parseInt('' + tpv + fir + fin + con, 2) << 4) + seq];
     },
 
     set_pwd = function (pwd) {
-        return [0x00];
+        if (_.isNumber(pwd)) {
+            return [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+        } else {
+            return [];
+        }
     },
 
     set_cs = function (data) {
-        return [0x00];
+        for (var i = 0, CS = 0; i < data.length; i++) CS += parseInt(data[i]);
+        return [CS % 256];
     },
 
     set_end = function () {
@@ -76,9 +93,9 @@ var json_hex = {
     };
 
 exports.json_hex = function (json) {
-    var a1 = json.A1, a2 = json.A2, a3 = json.A3,
-        c = set_c(), addr = set_addr(a1, a2, a3),
-        afn = json.AFN, seq = set_seq(json.seq),
+    var a1 = json.A1, a2 = json.A2, a3 = json.A3 || 0,
+        c = set_c(json), addr = set_addr(a1, a2, a3),
+        afn = json.AFN, seq = set_seq(json),
         pwd = set_pwd(json.AUX.PW), app = [], hex = [];
     _.each(json.DU, function (item) {
         var pn = item.pn;
@@ -88,7 +105,7 @@ exports.json_hex = function (json) {
             app = app.concat(du, json_hex['AFN' + afn]['Fn' + fn](data));
         });
     });
-    app = app.concat(c, addr, afn, seq, app, pwd);
+    app = [].concat(c, addr, afn, seq, app, pwd);
     hex = hex.concat(0x68, set_len(app), 0x68, app, set_cs(app), set_end());
     return hex;
 };
