4f059bfe8197347fff06cf43e01e9842291a935f
bright_xu@sina.cn <bright_xu@sina.cn>
2014-12-31 17:05:43 +0800

    698

--- tools.js
+++ tools.js
@@ -297,5 +297,216 @@ exports.tools = {
             dt2 = Math.floor((fn - 1) / 8);
         }
         return [dt1, dt2];
+    },
+
+    getDFA2: function (parameter1, parameter2) {
+        if (parameter1 == 0xee || parameter1 == 0xff || parameter2 == 0xee) return null;
+        var sfv = parseFloat(((this.bcd2b(parameter1) + (parameter2 % 16) * 100) * Math.pow(10, 4 - (parameter2 >> 5))).toFixed(3));
+        return sfv;
+    },
+
+    getDFA3: function (parameter1, parameter2, parameter3, parameter4) {
+        if (parameter1 == 0xee || parameter1 == 0xff || parameter2 == 0xee || parameter3 == 0xee || parameter4 == 0xee) return null;
+        return {
+            ene_value: (parameter4 >> 4) % 2 === 0 ? (this.bcd2b(parameter1) +
+            this.bcd2b(parameter2) * 100 + this.bcd2b(parameter3) * 10000 + (parameter4 % 16) * 1000000) :
+            0 - ((this.bcd2b(parameter1) +
+            this.bcd2b(parameter2) * 100 + this.bcd2b(parameter3) * 10000 + (parameter4 % 16) * 1000000)),
+            ene_unit: (parameter4 >> 6) % 2
+        };
+    },
+
+    getDFA4: function (parameter) {
+        if (parameter == 0xee || parameter == 0xff) return null;
+        return [this.bcd2b(parameter % 128), parameter >> 7 == 0 ? '上浮' : '下浮']
+    },
+
+    getDFA5: function (p1, p2) {
+        if (p1 == 0xee || p1 == 0xff || p2 == 0xee) return null;
+        var s = p2 >> 7,
+            r = this.bcd2b(p2 % 128 >> 4) * 100 + this.bcd2b(p2 % 16) * 10 + this.bcd2b(p1 >> 4) + this.bcd2b(p1 % 16) * 0.1;
+        if (s) r = 0 - r;
+        return parseFloat(r.toFixed(1));
+    },
+
+    getDFA6: function (parameter1, parameter2) {
+        if (parameter1 == 0xee || parameter1 == 0xff || parameter2 == 0xee) return null;
+        return (this.bcd2b(parameter1) * 0.01 + this.bcd2b(parameter2 % 128)).toFixed(2)
+    },
+
+    getDFA7: function (parameter1, parameter2) {
+        if (parameter1 == 0xee || parameter1 == 0xff || parameter2 == 0xee) return null;
+        return parseFloat((this.bcd2b(parameter1) * 0.1 + this.bcd2b(parameter2) * 10).toFixed(1));
+    },
+
+    getDFA9: function (p1, p2, p3) {
+        if (p1 == 0xee || p1 == 0xff || p2 == 0xee || p3 == 0xee) return null;
+        //s===0 正数 s===1 负数
+        var s = p3 >> 7,
+            r = this.bcd2b(p3 % 128 >> 4) * 10 + this.bcd2b(p3 % 16) + this.bcd2b(p2 >> 4) * .1 + this.bcd2b(p2 % 16) * .01 + this.bcd2b(p1 >> 4) * .001 + this.bcd2b(p1 % 16) * .0001;
+        if (s) r = 0 - r;
+        return parseFloat(r.toFixed(4));
+    },
+
+    getDFA10: function (p1, p2, p3) {
+        if (p1 == 0xee || p1 == 0xff || p2 == 0xee || p3 == 0xee) return null;
+        return (this.bcd2b(p3 >> 4) * 100000 +
+        this.bcd2b(p3 % 16) * 10000 +
+        this.bcd2b(p2 >> 4) * 1000 +
+        this.bcd2b(p2 % 16) * 100 +
+        this.bcd2b(p1 >> 4) * 10 +
+        this.bcd2b(p1 % 16));
+    },
+
+    getDFA11: function (p1, p2, p3, p4) {
+        if (p1 == 0xee || p1 == 0xff || p2 == 0xee || p3 == 0xee || p4 == 0xee) return null;
+        var r = this.bcd2b(p4 >> 4) * 100000 +
+            this.bcd2b(p4 % 16) * 10000 +
+            this.bcd2b(p3 >> 4) * 1000 +
+            this.bcd2b(p3 % 16) * 100 +
+            this.bcd2b(p2 >> 4) * 10 +
+            this.bcd2b(p2 % 16) +
+            this.bcd2b(p1 >> 4) * .1 +
+            this.bcd2b(p1 % 16) * .01;
+        return parseFloat(r.toFixed(2));
+    },
+
+    getDFA12: function (p1, p2, p3, p4, p5, p6) {
+        if (p1 == 0xee || p1 == 0xff || p2 == 0xee || p3 == 0xee || p4 == 0xee || p5 == 0xee) return null;
+        return (this.bcd2b(p6 >> 4) * 100000000000 +
+        this.bcd2b(p6 % 16) * 10000000000 +
+        this.bcd2b(p5 >> 4) * 1000000000 +
+        this.bcd2b(p5 % 16) * 100000000 +
+        this.bcd2b(p4 >> 4) * 10000000 +
+        this.bcd2b(p4 % 16) * 1000000 +
+        this.bcd2b(p3 >> 4) * 100000 +
+        this.bcd2b(p3 % 16) * 10000 +
+        this.bcd2b(p2 >> 4) * 1000 +
+        this.bcd2b(p2 % 16) * 100 +
+        this.bcd2b(p1 >> 4) * 10 +
+        this.bcd2b(p1 % 16));
+    },
+
+    getDFA13: function (p1, p2, p3, p4) {
+        if (p1 == 0xee || p1 == 0xff || p2 == 0xee || p3 == 0xee || p4 == 0xee) return null;
+        var r = this.bcd2b(p4 >> 4) * 1000 +
+            this.bcd2b(p4 % 16) * 100 +
+            this.bcd2b(p3 >> 4) * 10 +
+            this.bcd2b(p3 % 16) +
+            this.bcd2b(p2 >> 4) * .1 +
+            this.bcd2b(p2 % 16) * .01 +
+            this.bcd2b(p1 >> 4) * .001 +
+            this.bcd2b(p1 % 16) * .0001;
+        return parseFloat(r.toFixed(4));
+    },
+
+    getDFA14: function (p1, p2, p3, p4, p5) {
+        if (p1 == 0xee || p1 == 0xff || p2 == 0xee || p3 == 0xee || p4 == 0xee || p5 == 0xee) return null;
+        var r = this.bcd2b(p5 >> 4) * 100000 +
+            this.bcd2b(p5 % 16) * 10000 +
+            this.bcd2b(p4 >> 4) * 1000 +
+            this.bcd2b(p4 % 16) * 100 +
+            this.bcd2b(p3 >> 4) * 10 +
+            this.bcd2b(p3 % 16) +
+            this.bcd2b(p2 >> 4) * .1 +
+            this.bcd2b(p2 % 16) * .01 +
+            this.bcd2b(p1 >> 4) * .001 +
+            this.bcd2b(p1 % 16) * .0001;
+        return parseFloat(r.toFixed(4));
+    },
+
+    getDFA15: function (parameter1, parameter2, parameter3, parameter4, parameter5) {
+        if (parameter1 == 0xee || parameter1 == 0xff || parameter2 == 0xee || parameter3 == 0xee || parameter4 == 0xee || parameter5 == 0xee) return null;
+        return '20' + this.bcd2b(parameter5) + '/' + this.bcd2b(parameter4) + '/' +
+            this.bcd2b(parameter3) + ' ' + this.bcd2b(parameter2) + ':' +
+            this.bcd2b(parameter1);
+    },
+
+    getDFA16: function (p1, p2, p3, p4) {
+        if (p1 == 0xee || p1 == 0xff || p2 == 0xee || p3 == 0xee || p4 == 0xee) return null;
+        return this.bcd2b(p4) + ' ' + this.bcd2b(p3) + ':' + this.bcd2b(p2) + ':' + this.bcd2b(p1);
+    },
+
+    getDFA17: function (p1, p2, p3, p4) {
+        if (p1 == 0xee || p1 == 0xff || p2 == 0xee || p3 == 0xee || p4 == 0xee) return null;
+        return new Date().getFullYear() + '/' + this.bcd2b(p4) + '/' + this.bcd2b(p3) + ' ' + this.bcd2b(p2) + ':' + this.bcd2b(p1);
+    },
+
+    getDFA18: function (p1, p2, p3) {
+        if (p1 == 0xee || p1 == 0xff || p2 == 0xee || p3 == 0xee) return null;
+        return this.bcd2b(p3) + ' ' + this.bcd2b(p2) + ':' + this.bcd2b(p1);
+    },
+
+    getDFA19: function (parameter1, parameter2) {
+        if (parameter1 == 0xee || parameter1 == 0xff || parameter2 == 0xee) return null;
+        return this.bcd2b(parameter2) + '时' + this.bcd2b(parameter1) + '分'
+    },
+
+    getDFA20: function (parameter1, parameter2, parameter3) {
+        if (parameter1 == 0xee || parameter2 == 0xff || parameter2 == 0xee || parameter3 == 0xee) return null;
+        return (2000 + this.bcd2b(parameter3)) + '/' + this.bcd2b(parameter2) + '/' + this.bcd2b(parameter1)
+    },
+    getDFA21: function (parameter1, parameter2) {
+        if (parameter1 == 0xee || parameter2 == 0xff || parameter2 == 0xee ) return null;
+        return (2000 + this.bcd2b(parameter2)) + '/' + this.bcd2b(parameter1)
+    },
+    getDFA22: function (parameter) {
+        if (parameter == 0xee || parameter == 0xff) return null;
+        return (this.bcd2b(parameter) * 0.1).toFixed(1)
+    },
+
+    getDFA23: function (parameter1, parameter2, parameter3) {
+        if (parameter1 == 0xee || parameter1 == 0xff || parameter2 == 0xee || parameter3 == 0xee) return null;
+        return parseFloat((this.bcd2b(parameter1) * 0.0001 + this.bcd2b(parameter2) * 0.01 + this.bcd2b(parameter3)).toFixed(4));
+    },
+
+    getDFA25: function (parameter1, parameter2, parameter3) {
+        if (parameter1 == 0xee || parameter1 == 0xff || parameter2 == 0xee || parameter3 == 0xee) return null;
+        return parseFloat((this.bcd2b(parameter1) * 0.001 + this.bcd2b(parameter2) * 0.1 + this.bcd2b(parameter3 % 128) * 10).toFixed(3));
+    },
+
+    getDFA26: function (parameter1, parameter2) {
+        if (parameter1 == 0xee || parameter1 == 0xff || parameter2 == 0xee) return null;
+        return (this.bcd2b(parameter1) * 0.001 + this.bcd2b(parameter2) * 0.1).toFixed(3)
+    },
+
+    //********************************set**********************************
+
+    setDFA2: function (data) {
+        var arr = data.toString().split('.'), num, gnum, g,
+            decLen = (arr[1] || []).length;
+        if (decLen != 0) {
+            num = Math.floor(data * Math.pow(10, decLen));
+            gnum = Math.pow(10, 0 - decLen)
+        } else {
+            var tempNum = arr[0];
+            num = tempNum.substr(0, 3);
+            gnum = Math.pow(10, data.toString().length - num.length)
+        }
+        for (var i = 0; i < 8; i++) {
+            if (10000 * Math.pow(10, 0 - i) === gnum) {
+                g = i;
+                break;
+            }
+        }
+        return [this.b2bcd(num % 100), this.b2bcd(Math.floor(num / 100) % 100) + (g << 5)]
+    },
+
+    setDFA3: function (data) {
+        return [
+            this.b2bcd(data.ene_value[0] % 100),
+            this.b2bcd(Math.floor(data.ene_value[0] / 100) % 100),
+            this.b2bcd(Math.floor(data.ene_value[0] / 10000) % 100),
+            this.b2bcd(Math.floor(data.ene_value[0] / 1000000) % 100) + (data.ene_value[1] << 6)]
+
+    },
+
+    setDFA20: function (date) {
+        var dateArr = date.split('/');
+        return [
+            this.b2bcd(dateArr[1]),
+            this.b2bcd(dateArr[0]),
+            this.b2bcd(dateArr[2] - 2000)
+        ]
     }
 };
